929a821f945173ce56366123b45aa6a506524a11
SourabhIngle <100rabhingle@gmail.com>
2023-06-06 17:42:41 +0530

    Count number of roll dice

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -5,29 +5,20 @@ public class SnakeAndLadder {
     static int playerPosition = 0;
     static int dice = 0;
     static int option = 0;
-    static int noPlay = 1;
-    static int ladder = 2;
-    static int snake = 3;
-    static int win = 100;
-    static void gettingOptionSnakeLadderAndNoPlay() {
+    static final int noPlay = 1;
+    static final int ladder = 2;
+    static final int snake = 3;
+    static final int win = 100;
+
+    void gettingOptionSnakeLadderAndNoPlay() {
         option = (int) Math.floor(Math.random() * 10) % 3 + 1;
     }
-    static int rollDice()
-    {
+
+    void rollDice() {
         dice = (int) Math.floor(Math.random() * 10) % 6 + 1;
-        return dice ;
     }
 
-    static void ensureGettingExactPosition() {
-        if (playerPosition > 100) {
-            System.out.println("sorry, your dice is " + dice + " " +
-                    "and player position is " + playerPosition + " out of the win position" +
-                    " try again better luck next time to get the right number");
-            playerPosition = playerPosition - dice;
-        }
-    }
-
-    static void playerCondition() {
+    void playerCondition() {
         if (noPlay == option) {
             System.out.println("(NO-PLAY)=> NO LADDER, NO SNAKE, JUST POSITION");
             System.out.println();
@@ -63,17 +54,49 @@ public class SnakeAndLadder {
         System.out.println("-------------------------------------------------------------------------------------");
     }
 
-    public static void main(String[] args) {
-        System.out.println("Player position stating from " + playerPosition);
-        System.out.println("Player position stating from " + playerPosition);
-        while (playerPosition <= win) {
-            rollDice();
-            gettingOptionSnakeLadderAndNoPlay();
-            System.out.println("Dice roll is " + rollDice());
-            System.out.println("Your current Position is " + playerPosition);
-            playerCondition();
+    void ensureGettingExactPosition() {
+        if (playerPosition > 100) {
+            System.out.println("sorry, your dice is " + dice + " " +
+                    "and player position is " + playerPosition + " out of the win position" +
+                    " try again better luck next time to get the right number");
+            playerPosition = playerPosition - dice;
         }
+    }
 
+
+    public static void main(String[] args) {
+
+        int diceRollCount = 0;
+        System.out.println("Welcome to Snake and Ladder");
+        System.out.println("Player Position Stating from " + playerPosition);
+        com.sankeandladder.SnakeAndLadder method = new com.sankeandladder.SnakeAndLadder();
+
+        while (playerPosition <= win) {
+
+            //ROLL THE DICE GETTING 1 TO 6.
+            method.rollDice();
+            System.out.println("Dice roll is " + dice);
+            System.out.println("Your current Position is " + playerPosition);
+
+            //GETTING AN OPTION NO-PLAY ,SNAKE AND LADDER.
+            method.gettingOptionSnakeLadderAndNoPlay();
+
+            // FIRST ASSIGN PLAYER POSITION AFTER CHECK OPTIONS.
+            playerPosition = playerPosition + dice;
+
+            // HERE WE ARE CHECKING CONDITION PLAYER SNAKE , LADDER AND NO_PLAY.
+            method.playerCondition();
+
+
+            method.ensureGettingExactPosition();
+
+            if (playerPosition == win) {
+                System.out.println("Yes, you have reached the position is " + playerPosition + " .You are the winner.");
+                break;
+            }
+            diceRollCount++;
+        }
+        System.out.println("Total dice rolls is " + diceRollCount);
     }
 }
     
