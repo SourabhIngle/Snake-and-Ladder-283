8926183a198169c4a092cb5165ad58b5cb8d1e09
SourabhIngle <100rabhingle@gmail.com>
2023-06-06 17:21:51 +0530

    Player Straring Position 0

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,8 +1,12 @@
 package com.sankeandladder;
 
 public class SnakeAndLadder {
+
+    static int position = 0;
+
     public static void main(String[] args) {
-        System.out.println("Welcome to Snake and Ladder");
+
+        System.out.println("Player position stating from " + position);
     }
 }
-
+    
