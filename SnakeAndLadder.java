1c7d36bf689e6318ec48fa43ea4b80d2259c7116
SourabhIngle <100rabhingle@gmail.com>
2023-06-06 17:35:21 +0530

    Set option No play, Snake and Ladder

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -4,16 +4,62 @@ public class SnakeAndLadder {
 
     static int playerPosition = 0;
     static int dice = 0;
+    static int option = 0;
+    static int noPlay = 1;
+    static int ladder = 2;
+    static int snake = 3;
+    void gettingOptionSnakeLadderAndNoPlay() {
+        option = (int) Math.floor(Math.random() * 10) % 3 + 1;
+    }
     static int rollDice()
     {
         dice = (int) Math.floor(Math.random() * 10) % 6 + 1;
         return dice ;
     }
+    static void playerCondition() {
+        if (noPlay == option) {
+            System.out.println("(NO-PLAY)=> NO LADDER, NO SNAKE, JUST POSITION");
+            System.out.println();
+        }
+
+        if (ladder == option) {
+            System.out.println("YOU GOT A LADDER");
+            if (playerPosition + dice <= 100) {
+                playerPosition = playerPosition + dice;
+                System.out.println("+" + dice);
+                System.out.println("yes, You got a Ladder now your position is " + playerPosition);
+                System.out.println();
+            }
+        }
+
+        if (snake == option) {
+            playerPosition = playerPosition - (dice + dice);
+            if (playerPosition < 0) {
+                System.out.println("OH NO, SNAKE APPEAR");
+                System.out.println("It is not possible to assign " + playerPosition + " negative position ");
+                playerPosition = 0;
+                System.out.println("Now your position going to be " + playerPosition);
+                System.out.println();
+            }
+            if (playerPosition - dice < 100 && playerPosition - dice > 0) {
+                System.out.println("OH NO, SNAKE APPEAR");
+
+                System.out.print("-");
+                System.out.println(dice + dice);
+                System.out.println("Sorry, Snake bites you now your position " + playerPosition);
+            }
+        }
+        System.out.println("-------------------------------------------------------------------------------------");
+    }
 
     public static void main(String[] args) {
         System.out.println("Player position stating from " + playerPosition);
+
+        rollDice();
         System.out.println("Dice roll is " + rollDice());
         System.out.println("Your current Position is " + playerPosition);
+        playerCondition();
+
     }
 }
     
