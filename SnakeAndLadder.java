7ab2c23eeae1012b58c12f7a7f12aa12381b7a3a
SourabhIngle <100rabhingle@gmail.com>
2023-06-06 17:32:32 +0530

    Create dice roll

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -2,11 +2,18 @@ package com.sankeandladder;
 
 public class SnakeAndLadder {
 
-    static int position = 0;
+    static int playerPosition = 0;
+    static int dice = 0;
+    static int rollDice()
+    {
+        dice = (int) Math.floor(Math.random() * 10) % 6 + 1;
+        return dice ;
+    }
 
     public static void main(String[] args) {
-
-        System.out.println("Player position stating from " + position);
+        System.out.println("Player position stating from " + playerPosition);
+        System.out.println("Dice roll is " + rollDice());
+        System.out.println("Your current Position is " + playerPosition);
     }
 }
     
